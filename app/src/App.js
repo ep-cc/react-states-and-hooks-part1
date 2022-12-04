c4b344f14a1702834af35552c16ecd010ef46e17
Peter Erben <erben.peter@gmail.com>
2022-12-04 17:56:28 +0100

    add Coin component

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 
+
+const SIDES = {
+  UNSET: '?',
+  HEADS: 'H',
+  TAILS: 'T',
+};
+
 function App() {
   return (
     <div className="App">
+      <h1>Coin flipping game</h1>
+      <div>
+        <Coin what={SIDES.HEADS}/>
+        <Coin what={SIDES.TAILS}/>
+      </div>
     </div>
   );
+}
+
+function Coin(props) {
+
+  return <div className='coin'>{props.what}</div>
 }
 
 export default App;
